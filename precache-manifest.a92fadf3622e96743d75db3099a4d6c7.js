7defe7081017437cb1a078eed15804b08bc5d2df
pascalr <pascal.rainville@gmail.com>
2019-05-25 10:24:55 -0400

    Updates

---
+++ precache-manifest.a92fadf3622e96743d75db3099a4d6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f08ad6446a5a59c1c6",
+    "url": "/spreadsheet_website/static/css/main.5e2c21de.chunk.css"
+  },
+  {
+    "revision": "a4f08ad6446a5a59c1c6",
+    "url": "/spreadsheet_website/static/js/main.a4f08ad6.chunk.js"
+  },
+  {
+    "revision": "ba839e92349cea6e0237",
+    "url": "/spreadsheet_website/static/js/runtime~main.ba839e92.js"
+  },
+  {
+    "revision": "0b603c99fb6d6b289f85",
+    "url": "/spreadsheet_website/static/css/2.136ceac2.chunk.css"
+  },
+  {
+    "revision": "0b603c99fb6d6b289f85",
+    "url": "/spreadsheet_website/static/js/2.0b603c99.chunk.js"
+  },
+  {
+    "revision": "d135225a57d1e6c6cec1fccd728a7f6d",
+    "url": "/spreadsheet_website/index.html"
+  }
+];
