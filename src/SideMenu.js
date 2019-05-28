ea121ec80a33c6906087642efa9300fae82cc15e
pascalr <pascal.rainville@gmail.com>
2019-05-28 18:01:39 -0400

    ...

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -36,10 +36,34 @@ const ColorBox = withDispatch(props => {
 const ColorSideMenu = colorable('sideMenu', function ColorableSideMenu(props) {
   const style={...(props.style || {})}
   style.border = 'solid 3px black'
+  style.height = '100%'
+  style.position = 'absolute'
+  style.width = '200px'
   return <div style={style}>
-    <ColorBox color='red'/>
-    <ColorBox color='green'/>
-    <ColorBox color='blue'/>
+    <div style={{textAlign: 'center', fontWeight: 'bold'}}>Color menu</div>
+    <div style={{borderBottom: '2px solid black', display: 'flex', flexWrap: 'wrap',
+    borderTop: '2px solid black'}}>
+      <ColorBox color='maroon'/>
+      <ColorBox color='red'/>
+      <ColorBox color='orange'/>
+      <ColorBox color='yellow'/>
+      <ColorBox color='olive'/>
+      <ColorBox color='green'/>
+      <ColorBox color='purple'/>
+      <ColorBox color='fuchsia'/>
+      <ColorBox color='lime'/>
+      <ColorBox color='teal'/>
+      <ColorBox color='aqua'/>
+      <ColorBox color='blue'/>
+      <ColorBox color='navy'/>
+      <ColorBox color='black'/>
+      <ColorBox color='gray'/>
+      <ColorBox color='silver'/>
+      <ColorBox color='white'/>
+      <ColorBox color='whitesmoke'/>
+      <ColorBox color='gold'/>
+      <ColorBox color='tomato'/>
+    </div>
   </div>
 })
 
